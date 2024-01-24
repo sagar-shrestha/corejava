96e0562ed89a382f52e7f059a61876f8efbc4b18
DESKTOP-GL871V8\sagar <sagarshrestha3111@gmail.com>
2024-01-24 12:52:42 +0545

    hello

--- src/FrequencyOfNumberInArray.java
+++ src/FrequencyOfNumberInArray.java
@@ -1,5 +1,31 @@
 public class FrequencyOfNumberInArray {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 4, 5, 2, 3, 6, 5};
+        int visited = -1;
+
+        for (int i = 0; i < array.length; i++) {
+            int count = 0;
+            boolean matched = false;
+            boolean firstTime = true;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j] && array[j] != visited) {
+                    count++;
+                    if (matched) {
+                        array[j] = visited;
+                    }
+                    if (firstTime) {
+                        matched = true;
+                        firstTime = false;
+                    }
+                }
+            }
+            if (array[i] != visited) {
+                System.out.println("The frequency of " + array[i] + " is: " + count);
+            }
+            array[i] = visited;
+        }
+
     }
 }
